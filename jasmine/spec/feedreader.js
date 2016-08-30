83f1ac7b8f9204f01f19c8ea42d18eee5b40e0f5
Anton Matrosov <antmatr@yandex.ru>
2016-08-30 22:56:05 +0300

    Test: Refactor 'New Feed Selection' async test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,9 +27,8 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* This test loops through each feed in the allFeeds object
+         * and ensures it has a URL defined and not empty.
          */
         it('URLs are defined and not empty', function () {
             allFeeds.forEach(function (feed) {
@@ -39,9 +38,8 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* This test loops through each feed in the allFeeds object
+         * and ensures it has a name defined and not empty.
          */
         it('names are defined and not empty', function () {
             allFeeds.forEach(function (feed) {
@@ -51,28 +49,17 @@ $(function () {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function () {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        // This test ensures the menu element is hidden by default.
         it('is hidden by default', function () {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-        /* TODO: Write a test that ensures the menu changes
-         * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
+        /* This test ensures the menu changes visibility
+         * when the menu icon is clicked.
          */
         it('changes visibility when the menu icon is clicked', function () {
             var menuIcon = $('.menu-icon-link');
-
-            // Not sure if using of 'trigger' method
-            // is the right way for testing
-            // it seems like an easy solution for this task
             menuIcon.trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(false);
             menuIcon.trigger('click');
@@ -80,46 +67,34 @@ $(function () {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function () {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This test ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
         beforeEach(function (done) {
-            loadFeed(0, function () {
-                done();
-            });
+            loadFeed(0, done);
         });
 
-        it('have at least a single .entry', function (done) {
-            expect($('.feed > .entry-link').length).not.toBe(0);
-            done();
+        it('have at least a single .entry', function () {
+            expect($('.feed .entry').length).not.toBe(0);
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection"*/
     describe('New Feed Selection', function () {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* This test ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
          */
 
-        // This is the only one test suite made me nervous
-        // I am still not sure that my solution is efficient:
-        // making two requests and saving each jQuery-data
-        // and then comparing them to each other
         var result1;
         var result2;
         beforeEach(function (done) {
             loadFeed(0, function () {
-                result1 = $('.feed > .entry-link');
-            });
-            loadFeed(1, function () {
-                result2 = $('.feed > .entry-link');
-                done();
+                result1 = $('.feed .entry').html();
+                loadFeed(1, function () {
+                    result2 = $('.feed .entry').html();
+                    done();
+                });
             });
         });
 
